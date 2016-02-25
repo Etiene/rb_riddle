dd217f86daf3a573e84d3ecc066d5a1f1b2d2ff9
Etiene Dalcol <dalcol@etiene.net>
2016-02-26 00:42:49 +0100

    Add some checks and reorganize

--- main.lua
+++ main.lua
@@ -1,33 +1,16 @@
 local MAX = 50
 local boundaries = {}
-local last_seen = {} -- Points where robots were last seen before vanishing off grid and left a "scent"
+
+local function max_check(x,y)
+	if x > MAX or y > MAX then
+		error('Error, the maximum value for a coordinate is '..MAX)
+	end
+end
 
 local function set_boundaries(x,y)
-	boundaries.x = x <= MAX and x or MAX
-	boundaries.y = y <= MAX and y or MAX
-end
-
-local Robot = {}
-local mt_rbt = { __index = Robot}
-
-local function get_index(t,str)
-	for i, v in ipairs(t) do 
-		if v == str then return i end
-	end
-	return nil
-end
-
-local orientations = {"N", "E", "S", "W"} -- <--- left ... turn ... right---->
-
-function Robot:turn(direction)
-	local i = get_index(orientations,self.orientation)
-	local new_i 
-	if direction == "L" then
-		new_i = i == 1 and #orientations or i - 1
-	elseif direction == "R" then
-		new_i = (i % #orientations) + 1
-	end
-	self.orientation = orientations[new_i]
+	max_check(x,y)
+	boundaries.x = x 
+	boundaries.y = y
 end
 
 local function is_off_boundaries(x,y)
@@ -40,6 +23,55 @@ local function is_off_boundaries(x,y)
 	end	
 	return false
 end
+
+local function get_index(t,str)
+	for i, v in ipairs(t) do 
+		if v == str then return i end
+	end
+	return nil
+end
+
+
+
+local Robot = {}
+local mt_rbt = { __index = Robot}
+local last_seen = {} -- Points where robots were last seen before vanishing off grid and left a "scent"
+local orientations = {"N", "E", "S", "W"} -- <--- left ... turn ... right---->
+
+function Robot:new(x,y,orientation)
+	local obj = {
+		position = { x = x, y = y },
+		orientation = orientation,
+		lost = false 
+	}
+	if is_off_boundaries(x,y) then
+		obj.lost = true
+	end
+	setmetatable(obj,mt_rbt)
+	return obj
+end
+
+-- direction: string L, R or F
+function Robot:move(direction)
+	if self.lost then return end 
+	if direction == "F" then
+		self:walk()
+	else
+		self:turn(direction)
+	end
+end
+
+function Robot:turn(direction)
+	local i = get_index(orientations,self.orientation)
+	local new_i 
+	if direction == "L" then
+		new_i = i == 1 and #orientations or i - 1
+	elseif direction == "R" then
+		new_i = (i % #orientations) + 1
+	end
+	self.orientation = orientations[new_i]
+end
+
 
 function Robot:walk()
 	local new_x, new_y
@@ -66,57 +98,30 @@ function Robot:walk()
 	self.position.x, self.position.y = new_x, new_y
 end
 
-
-
--- x,y < MAX
--- orientation: string, N,S,E or W
-function Robot:new(x,y,orientation)
-	local obj = {
-		position = { x = x, y = y },
-		orientation = orientation,
-		lost = false 
-	}
-	setmetatable(obj,mt_rbt)
-	return obj
-end
-
- -- direction: string L, R or F
-function Robot:move(direction)
-	if self.lost then return end 
-	if direction == "F" then
-		self:walk()
-	else
-		self:turn(direction)
-	end
-end
-
 function Robot:get_scent()
 	for _,p in ipairs(last_seen) do
 		if p.x == self.position.x and p.y == self.position.y then
-			print("I stepped in a bad place")
 			return true
 		end
 	end
 	return false
 end 
 
--- allow additional commands...
+
+-- Main Loop for reading input file
 local fp = io.open("input.txt","r")
 local b_x, b_y = fp:read("*number","*number")
 set_boundaries(b_x,b_y)
 
-print(b_x, b_y)
 
 while(fp:read(0)) do
 	local x, y, _, o = fp:read("*number","*number",1,1) -- _ for whitespace
+	max_check(x,y)
 	local r = Robot:new(x,y,o)
-	print(r.position.x,r.position.y,r.orientation)
-
+	
 	fp:read("*line") -- reads the rest of the line
 
 	local instructions = fp:read("*line")
-	print(instructions)
-
 	instructions:gsub(".", function(c) r:move(c) end)
 
 	print(r.position.x,r.position.y,r.orientation,r.lost and 'LOST' or '')
