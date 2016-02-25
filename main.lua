48124d52009773149dccf8db7dd7e590f335ac52
Etiene Dalcol <dalcol@etiene.net>
2016-02-26 00:02:44 +0100

    Add boundaries and refactor

--- main.lua
+++ main.lua
@@ -1,19 +1,10 @@
 local MAX = 50
 
-local Point = {}
-local mt_pt = {	__index = Point}
-
-function Point:new(x, y)
-	local obj = {x = x, y = y}
-	setmetatable(obj,mt_pt)
-	return obj
+local boundaries = {}
+local function set_boundaries(x,y)
+	boundaries.x = x <= MAX and x or MAX
+	boundaries.y = y <= MAX and y or MAX
 end
-
-function Point:update(x,y)
-	self.x = x and x or self.x -- keeps self.x if x is nil, first boolean op will be false
-	self.y = y and y or self.y
-end
-
 
 local Robot = {}
 local mt_rbt = { __index = Robot}
@@ -40,6 +31,7 @@ function Robot:turn(direction)
 end
 
 function Robot:walk()
+	local x_now, y_now = self.position.x, self.position.y
 	if self.orientation == "N" then
 		self.position.y = self.position.y + 1
 	elseif self.orientation == "E" then
@@ -49,14 +41,27 @@ function Robot:walk()
 	elseif self.orientation == "W" then
 		self.position.x = self.position.x - 1
 	end
+	if self:is_off_boundaries() then self.lost = true end
+end
+
+function Robot:is_off_boundaries()
+	if  self.position.x < 0 or
+		self.position.x > boundaries.x or 
+		self.position.y < 0 or 
+		self.position.y > boundaries.y then
+
+		return true
+	end	
+	return false
 end
 
 -- x,y < MAX
 -- orientation: string, N,S,E or W
 function Robot:new(x,y,orientation)
 	local obj = {
-		position = Point:new(x,y),
-		orientation = orientation 
+		position = { x = x, y = y },
+		orientation = orientation,
+		lost = false 
 	}
 	setmetatable(obj,mt_rbt)
 	return obj
@@ -64,6 +69,8 @@ end
 
  -- direction: string L, R or F
 function Robot:move(direction)
+	if self.lost then return end 
+
 	if direction == "F" then
 		self:walk()
 	else
@@ -74,31 +81,29 @@ end
 -- Points where robots were last seen before vanishing off grid and left a "scent"
 local last_seen = {}
 
--- Size of grid, from (0,0) to (boundaries.x, boundaries.y)
--- First input
-local boundaries = {}
-
-local instruction = ""
-
 -- allow additional commands...
 local fp = io.open("input.txt","r")
 local b_x, b_y = fp:read("*number","*number")
+set_boundaries(b_x,b_y)
 
 print(b_x, b_y)
 
-local x, y, _, o = fp:read("*number","*number",1,1) -- _ for whitespace
-local r = Robot:new(x,y,o)
-print(r.position.x,r.position.y,r.orientation)
+while(fp:read(0)) do
+	local x, y, _, o = fp:read("*number","*number",1,1) -- _ for whitespace
+	local r = Robot:new(x,y,o)
+	print(r.position.x,r.position.y,r.orientation)
 
-fp:read("*line") -- reads the rest of the line
+	fp:read("*line") -- reads the rest of the line
 
-local instructions = fp:read("*line")
-print(instructions)
+	local instructions = fp:read("*line")
+	print(instructions)
 
-instructions:gsub(".", function(c) r:move(c) end)
+	instructions:gsub(".", function(c) r:move(c) end)
 
-print(r.position.x,r.position.y,r.orientation)
+	print(r.position.x,r.position.y,r.orientation,r.lost and 'LOST' or '')
 
+	fp:read("*line") -- reads a new line, should be blank
+end 
 fp:close()
 
 
